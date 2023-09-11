4515cd4ccaf0bba0bbd42d5cb9e8e9e5c3acf7d7
XandP <alexandre.rochap@hotmail.com>
2023-09-11 14:39:28 -0300

    Feat: Testes com variáveis.

--- basico/main.rs
+++ basico/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello World");
+    let variavel:i32 = 300;
+
+    println!("Variavel = {}, Tamanho = {} bytes", variavel, std::mem::size_of_val(&variavel));
+
+    let decimal:f32 = 2.5;
+    println!("Decimal = {}", decimal);
+
+    let mut booleana = false;
+    booleana = true;
+    println!("Booleana = {}, Tamanho = {} bytes", booleana, std::mem::size_of_val(&booleana));
+
+    let letra:char = 'C';
+    println!("Tamanho char = {}", std::mem::size_of_val(&letra));
 }
